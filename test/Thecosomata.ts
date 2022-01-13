9509cdc7c74c1fc01a035d765d95d68beb9c3dc3
Khoa Phan <khoaphan89@gmail.com>
2022-01-13 11:05:03 -0600

    Fix mock contract and tests

--- test/Thecosomata.ts
+++ test/Thecosomata.ts
@@ -15,6 +15,12 @@ describe('Thecosomata', function () {
     const sushiV2FactoryAddr: string =
       '0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac';
     const ohmAddr: string = '0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5';
+    const sOhmAddr: string = '0x04906695D6D12CF5459975d7C3C03356E4Ccd460';
+    const olympusTreasury: string =
+      '0x9a315bdf513367c0377fb36545857d12e85813ef';
+    const redactedTreasury: string =
+      '0x086C98855dF3C78C6b481b6e1D47BeF42E9aC36B';
+    const sushiRouter: string = '0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f';
 
     // NOTE: We are using ThecosomataInternal in order to test internal methods
     const Thecosomata = await ethers.getContractFactory('ThecosomataInternal');
@@ -23,7 +29,11 @@ describe('Thecosomata', function () {
     thecosomata = await Thecosomata.deploy(
       btrflyAddr,
       sushiV2FactoryAddr,
-      ohmAddr
+      ohmAddr,
+      sOhmAddr,
+      olympusTreasury,
+      redactedTreasury,
+      sushiRouter
     );
     btrfly = await getBTRFLY(btrflyAddr);
   });
@@ -43,7 +53,7 @@ describe('Thecosomata', function () {
       // Quick and dirty method of getting BTRFLY to Thecosomata
       const btrflyDonor = await impersonateAddressAndReturnSigner(
         admin,
-        '0x9e3421274fb4053a83917d62bd368332e9e71fe0' // Replace with address with BTRFLY balance if needed
+        '0x55b2d13031eb53831e3bb30a67f8d810e85a3d45' // Replace with address with BTRFLY balance if needed
       );
 
       await btrfly
@@ -58,7 +68,9 @@ describe('Thecosomata', function () {
 
   describe('calculateOHMAmountRequiredForLP', () => {
     it('Should calculate the amount of OHM required for pairing with the BTRFLY balance', async () => {
-      const ohm = (await thecosomata._calculateOHMAmountRequiredForLP()).toNumber();
+      const ohm = (
+        await thecosomata._calculateOHMAmountRequiredForLP()
+      ).toNumber();
 
       expect(ohm.valueOf()).to.be.greaterThan(0);
 
