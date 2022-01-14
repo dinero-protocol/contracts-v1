2da5afcce488b9fc6a156e1643a46f34cc0eb408
Khoa Phan <khoaphan89@gmail.com>
2022-01-14 15:05:14 -0600

    Assign numeric values to variables for reuse

--- test/Thecosomata.ts
+++ test/Thecosomata.ts
@@ -3,7 +3,15 @@ import { ethers } from 'hardhat';
 import { BigNumber } from 'ethers';
 import { expect } from 'chai';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { BTRFLY, ThecosomataInternal, OlympusERC20Token, SOlympus, OlympusTreasury, SwapRouter, REDACTEDTreasury } from '../typechain';
+import {
+  BTRFLY,
+  ThecosomataInternal,
+  OlympusERC20Token,
+  SOlympus,
+  OlympusTreasury,
+  SwapRouter,
+  REDACTEDTreasury,
+} from '../typechain';
 
 describe('Thecosomata', function () {
   let admin: SignerWithAddress;
@@ -14,6 +22,14 @@ describe('Thecosomata', function () {
   let ohmTreasury: OlympusTreasury;
   let swapRouter: SwapRouter;
   let redactedTreasury: REDACTEDTreasury;
+
+  const ohmMintForRouter: number = 10e18;
+  const sOhmMintForAdmin: number = 200e18;
+  const redactedTreasuryOhmFloor = 5e9;
+  const redactedTreasurySOhmFloor = 5e9;
+  const redactedTreasurySOhmDeposit = 100e18;
+  const adminBtrflyReceivedForSOhmDeposit: number = 10e9;
+  const olympusTreasuryDebtLimit: number = redactedTreasurySOhmDeposit / 2;
 
   before(async () => {
     const sushiV2FactoryAddr: string =
@@ -26,7 +42,9 @@ describe('Thecosomata', function () {
     const BTRFLY = await ethers.getContractFactory('BTRFLY');
     const OHMTreasury = await ethers.getContractFactory('OlympusTreasury');
     const SwapRouter = await ethers.getContractFactory('SwapRouter');
-    const REDACTEDTreasury = await ethers.getContractFactory('REDACTEDTreasury');
+    const REDACTEDTreasury = await ethers.getContractFactory(
+      'REDACTEDTreasury'
+    );
     // NOTE: We are using ThecosomataInternal in order to test internal methods
     const Thecosomata = await ethers.getContractFactory('ThecosomataInternal');
 
@@ -44,7 +62,7 @@ describe('Thecosomata', function () {
       admin.address, // placeholder for cvx, not used here
       admin.address, // placeholder for crv, not used here
       admin.address, // placeholder for bond, not used here
-      (5e9).toString(), // ohm floor
+      redactedTreasuryOhmFloor.toString(), // ohm floor
       0, // cvx floor
       0, // crv floor
       0
@@ -56,16 +74,25 @@ describe('Thecosomata', function () {
       sOhm.address,
       ohmTreasury.address,
       redactedTreasury.address,
-      sushiV2RouterAddr,
+      sushiV2RouterAddr
     );
 
-    const setSOHMFloorTx = await redactedTreasury.setFloor(sOhm.address, (5e9).toString());
+    const setSOHMFloorTx = await redactedTreasury.setFloor(
+      sOhm.address,
+      redactedTreasurySOhmFloor.toString()
+    );
     await setSOHMFloorTx.wait();
 
     // Mint some test tokens for testing
-    const ohmMintTx = await ohm.mint(swapRouter.address, (10e18).toString());
+    const ohmMintTx = await ohm.mint(
+      swapRouter.address,
+      ohmMintForRouter.toString()
+    );
     await ohmMintTx.wait();
-    const sOhmMintTx = await sOhm.mint(admin.address, (200e18).toString());
+    const sOhmMintTx = await sOhm.mint(
+      admin.address,
+      sOhmMintForAdmin.toString()
+    );
     await sOhmMintTx.wait();
 
     // Unfreeze btrfly
@@ -75,28 +102,57 @@ describe('Thecosomata', function () {
     // Deposit some sOHM to mock the treasury reserves as a reserve manager
     const setVaultTx = await btrfly.setVault(redactedTreasury.address);
     await setVaultTx.wait();
-    const queueReserveManagerTx = await redactedTreasury.queue(0, admin.address);
+    const queueReserveManagerTx = await redactedTreasury.queue(
+      0,
+      admin.address
+    );
     await queueReserveManagerTx.wait();
-    const toggleReserveManagerTx = await redactedTreasury.toggle(0, admin.address, admin.address);
+    const toggleReserveManagerTx = await redactedTreasury.toggle(
+      0,
+      admin.address,
+      admin.address
+    );
     await toggleReserveManagerTx.wait();
-    const approveDepositTx = await sOhm.approve(redactedTreasury.address, (100e18).toString());
+    const approveDepositTx = await sOhm.approve(
+      redactedTreasury.address,
+      redactedTreasurySOhmDeposit.toString()
+    );
     await approveDepositTx.wait();
-    const depositTx = await redactedTreasury.deposit((100e18).toString(), sOhm.address, (10e9).toString());
+    const depositTx = await redactedTreasury.deposit(
+      redactedTreasurySOhmDeposit.toString(),
+      sOhm.address,
+      adminBtrflyReceivedForSOhmDeposit.toString()
+    );
     await depositTx.wait();
 
-    const sendBtrflyToRouterTx = await btrfly.transfer(swapRouter.address, (5e9).toString());
+    const sendBtrflyToRouterTx = await btrfly.transfer(
+      swapRouter.address,
+      (5e9).toString() // TO DO: Set as variable (awaiting answer)
+    );
     await sendBtrflyToRouterTx.wait();
 
     // Create and initialize the starting ratio between OHM and BTRFLY (5:1 for testing purpose)
-    const initPairTx = await swapRouter.init(ohm.address, btrfly.address, (5e18).toString(), (1e9).toString(), admin.address);
+    const initPairTx = await swapRouter.init(
+      ohm.address,
+      btrfly.address,
+      (5e18).toString(), // TO DO: Set as variable (awaiting answer)
+      (1e9).toString(), // TO DO: Set as variable (awaiting answer)
+      admin.address
+    );
     await initPairTx.wait();
 
     // Set debt limit for thecosomata
-    const setDebtLimitTx = await ohmTreasury.setDebtLimit(thecosomata.address, (5e18).toString());
+    const setDebtLimitTx = await ohmTreasury.setDebtLimit(
+      thecosomata.address,
+      olympusTreasuryDebtLimit.toString()
+    );
     await setDebtLimitTx.wait();
 
     // Give permission to thecosomata contract for reserve-management permission (ENUM #3)
-    const queueManagerPermissionTx = await redactedTreasury.queue(3, thecosomata.address);
+    const queueManagerPermissionTx = await redactedTreasury.queue(
+      3,
+      thecosomata.address
+    );
     await queueManagerPermissionTx.wait();
     const toggleManagerPermissionTx = await redactedTreasury.toggle(
       3,
@@ -116,7 +172,10 @@ describe('Thecosomata', function () {
     });
 
     it('Should request upkeep if BTRFLY balance is above 0', async () => {
-      const sendBtrflyTx = await btrfly.transfer(thecosomata.address, (1e9).toString());
+      const sendBtrflyTx = await btrfly.transfer(
+        thecosomata.address,
+        (1e9).toString()
+      );
       await sendBtrflyTx.wait();
 
       const [upkeepNeeded] = await thecosomata.checkUpkeep(new Uint8Array());
@@ -127,7 +186,8 @@ describe('Thecosomata', function () {
 
   describe('calculateOHMAmountRequiredForLP', () => {
     it('Should calculate the amount of OHM required for pairing with the BTRFLY balance', async () => {
-      const ohm: BigNumber = await thecosomata._calculateOHMAmountRequiredForLP();
+      const ohm: BigNumber =
+        await thecosomata._calculateOHMAmountRequiredForLP();
 
       expect(ohm.gt(0)).to.equal(true);
     });
@@ -139,7 +199,11 @@ describe('Thecosomata', function () {
       await thecosomata.performUpkeep(new Uint8Array());
 
       // Check LP-token balance
-      const lpBalance = await swapRouter.lpBalance(redactedTreasury.address, ohm.address, btrfly.address);
+      const lpBalance = await swapRouter.lpBalance(
+        redactedTreasury.address,
+        ohm.address,
+        btrfly.address
+      );
       expect(lpBalance.gt(0)).to.equal(true);
     });
   });
