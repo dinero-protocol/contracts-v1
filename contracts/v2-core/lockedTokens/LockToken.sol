d71b4df1cabefa934c2b4fed420c7087e99a7232
RealKinando <kiranfernando@yahoo.com>
2022-02-05 14:44:14 +0000

    structure for lockedToken implementation

---
+++ contracts/v2-core/lockedTokens/LockToken.sol
@@ -0,0 +1,17 @@
+// import solmate erc1155
+// storage of collateral in vault
+// delegate voting power
+// lock types
+// nft specific info
+// - expiration
+// - avgDepositTime
+// - depositAmount
+// - lockType
+// contract wide variables
+// - cumulative conviction
+// rageQuit - rage quit calculator
+// store voting power on-chain, custom transfer hooks
+// - snapshots of users votes
+// - logic to deal with locks (validUntil)
+
+// events for deposits, locks etc
---
+++ contracts/v2-core/lockedTokens/LockedVault.sol
@@ -0,0 +1,5 @@
+// stores locked BTRFLY
+// only authorised parties can call this
+// - multisig
+// - lockTokenContract
+// has method to allow transfer of tokens accidentally sent to it (only multisig)
