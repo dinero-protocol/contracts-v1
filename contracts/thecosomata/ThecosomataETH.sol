88fcfbf531eb2c8355f4c101ad09a586fd7a4bce
Eko Mirhard <eko.mirhard@gmail.com>
2022-02-10 11:58:45 +0700

    Remove console

--- contracts/thecosomata/ThecosomataETH.sol
+++ contracts/thecosomata/ThecosomataETH.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "hardhat/console.sol";
 
 interface IBTRFLY is IERC20 {
     function burn(uint256 amount) external;
